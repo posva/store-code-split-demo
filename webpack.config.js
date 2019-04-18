1723854e624c044b83138371656232e11e9dc8e6
Eduardo San Martin Morote <posva13@gmail.com>
2019-04-18 10:22:18 +0200

    simplify

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,6 @@
-const { resolve } = require('path')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-
-const outputPath = resolve(__dirname, 'dist')
-
 module.exports = env => ({
-  entry: resolve(__dirname, 'src/index.js'),
   mode: env.mode,
   devtool: 'inline-source-map',
-  output: {
-    path: outputPath,
-    // publicPath: '/',
-    filename: 'bundle.js',
-  },
 
   module: {
     rules: [
@@ -22,18 +10,4 @@ module.exports = env => ({
       },
     ],
   },
-
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-
-  plugins: [
-    // new HtmlWebpackPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV || env.mode),
-      },
-    }),
-  ],
 })
